7e9f5a38cd0b3a5449ee1690cd06dcac7a455634
Meuge <salsalito@gmail.com>
2012-03-01 17:17:37 +0100

    mich

--- gen/fr/activity/meuge/R.java
+++ gen/fr/activity/meuge/R.java
@@ -8,35 +8,39 @@
 package fr.activity.meuge;
 
 public final class R {
+    public static final class anim {
+        public static final int zoom_enter=0x7f040000;
+        public static final int zoom_exit=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int blanc=0x7f040001;
-        public static final int couleurMessage=0x7f040000;
+        public static final int blanc=0x7f050001;
+        public static final int couleurMessage=0x7f050000;
     }
     public static final class dimen {
-        public static final int dimMessage=0x7f050000;
+        public static final int dimMessage=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int robot=0x7f020001;
     }
     public static final class id {
-        public static final int btn_login=0x7f080007;
-        public static final int buttonBack=0x7f080002;
-        public static final int deconnection=0x7f08000c;
-        public static final int gallery=0x7f080004;
-        public static final int iback=0x7f08000e;
-        public static final int icontext=0x7f08000d;
-        public static final int imagefull=0x7f080001;
-        public static final int imageview=0x7f080003;
-        public static final int lbl_password=0x7f080009;
-        public static final int lbl_result=0x7f08000b;
-        public static final int lbl_top=0x7f08000a;
-        public static final int lbl_username=0x7f080008;
-        public static final int myGrid=0x7f080000;
-        public static final int txt_password=0x7f080006;
-        public static final int txt_username=0x7f080005;
+        public static final int btn_login=0x7f090007;
+        public static final int buttonBack=0x7f090002;
+        public static final int deconnection=0x7f09000c;
+        public static final int gallery=0x7f090004;
+        public static final int iback=0x7f09000e;
+        public static final int icontext=0x7f09000d;
+        public static final int imagefull=0x7f090001;
+        public static final int imageview=0x7f090003;
+        public static final int lbl_password=0x7f090009;
+        public static final int lbl_result=0x7f09000b;
+        public static final int lbl_top=0x7f09000a;
+        public static final int lbl_username=0x7f090008;
+        public static final int myGrid=0x7f090000;
+        public static final int txt_password=0x7f090006;
+        public static final int txt_username=0x7f090005;
     }
     public static final class layout {
         public static final int grid=0x7f030000;
@@ -46,19 +50,19 @@ public final class R {
         public static final int tableaux=0x7f030004;
     }
     public static final class menu {
-        public static final int menu=0x7f070000;
+        public static final int menu=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int bouton=0x7f060008;
-        public static final int bouton_retour=0x7f060009;
-        public static final int deconnection=0x7f060001;
-        public static final int entretonprenom=0x7f060005;
-        public static final int inscript=0x7f060007;
-        public static final int inscription=0x7f060006;
-        public static final int login=0x7f060002;
-        public static final int password=0x7f060003;
-        public static final int password2=0x7f060004;
+        public static final int app_name=0x7f070000;
+        public static final int bouton=0x7f070008;
+        public static final int bouton_retour=0x7f070009;
+        public static final int deconnection=0x7f070001;
+        public static final int entretonprenom=0x7f070005;
+        public static final int inscript=0x7f070007;
+        public static final int inscription=0x7f070006;
+        public static final int login=0x7f070002;
+        public static final int password=0x7f070003;
+        public static final int password2=0x7f070004;
     }
     public static final class styleable {
         /** Attributes that can be used with a GalleryTheme.
